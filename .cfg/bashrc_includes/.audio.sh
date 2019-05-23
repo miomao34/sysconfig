d9efd1ee00846cc2d68124848cb143930ef38c6d
miomao34 <flex384@gmail.com>
2019-05-23 23:39:13 +0300

    volume controls to console

--- .cfg/bashrc_includes/.audio.sh
+++ .cfg/bashrc_includes/.audio.sh
@@ -4,3 +4,7 @@ alias audiojack='amixer cset numid=3 1'
 
 # mute/unmute
 alias audiomute='amixer -q sset PCM toggle'
+
+# increase/decrease volume by 5%
+alias audioup='amixer -M sset PCM 5%+'
+alias audiodown='amixer -M sset PCM 5%-'
