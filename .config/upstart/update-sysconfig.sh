5b27000acfa035b9d09da01e0797e204926cb531
miomao34 <flex384@gmail.com>
2019-06-24 13:38:26 +0300

    added autoinstall script; about to make git.io link

---
+++ .config/upstart/update-sysconfig.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ~
+rm -rf ./sysconfig/
+rm -rf ./.git
+
+git clone --no-checkout git@github.com:miomao34/sysconfig.git
+mv ./sysconfig/.git .
+rm -rf ./sysconfig/
+
+git checkout HEAD
