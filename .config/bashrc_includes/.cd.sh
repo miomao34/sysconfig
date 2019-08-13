f4183afdff0f57d48f54a6713264ec0d48bcb811
miomao34 <flex384@gmail.com>
2019-08-13 19:16:39 +0300

    added functionality to cd: go up as many times as '+' was written in first and only cd arg

--- .config/bashrc_includes/.cd.sh
+++ .config/bashrc_includes/.cd.sh
@@ -7,8 +7,20 @@ unalias cd &> /dev/null
 
 cdi()
 {
+	backward_symbol="+"
 	clear
-	cd "$@"
+	symbols_count=$(awk -F"$backward_symbol" '{print NF-1}' <<< "${1}")
+	
+	if [ "$symbols_count" -ge "1" ] && [ -z "${1//\+}" ] && [ "$1" ]
+	then
+		current_dir="$(pwd)"
+		
+		for i in $(eval echo "{1..$symbols_count}"); do cd ..; done
+		
+		export OLDPWD="$current_dir"
+	else	
+		cd "$@"
+	fi
 	lsa
 }
 
