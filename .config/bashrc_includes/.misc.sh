0e6f96c50e3235208ebe4044673dfdd13cf1795e
miomao34 <flex384@gmail.com>
2019-09-01 22:49:11 +0300

    added unc for checking if current session is an ssh session

--- .config/bashrc_includes/.misc.sh
+++ .config/bashrc_includes/.misc.sh
@@ -23,3 +23,45 @@ sdn()
 	fi
 }
 
+# get ssh connection stack
+is_ssh()
+{
+	ssh_stack="$SSH_CONNECTION"
+	if [ ! "$ssh_stack" ]
+	then
+		echo "current session is not a SSH session"
+		return 0
+	fi
+	
+	counter=0
+	home=""
+	for word in $ssh_stack
+	do
+		if [ ! $counter -eq 0 ]
+		then
+			home="$word"
+		fi
+
+		if [ ! $counter -eq 0 ] && [ $((counter%2)) -eq 0 ]
+		then
+			echo
+		fi
+
+		if [ $((counter%2)) -eq 0 ]
+		then
+			echo -en "${BLUE}$word${NC} @ "
+		else
+			echo -en "${YELLOW}$word${NC}"
+		fi
+
+		if [ $counter -eq 1 ]
+		then
+			echo -en "${GREEN} (HOME)${NC}"	
+		fi
+
+		counter=$(expr $counter + 1)
+	done
+
+	echo -e "${RED} (THIS MACHINE)${NC}"
+
+}
