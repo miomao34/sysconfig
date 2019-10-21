b3ea8c69b3e402cecafb3becb3f9201176233b8b
miomao34 <flex384@gmail.com>
2019-10-21 20:51:08 +0300

    now ls won't surround spaced files and dirs with single quotes

--- .config/bashrc_includes/.cd_and_ls.sh
+++ .config/bashrc_includes/.cd_and_ls.sh
@@ -1,6 +1,6 @@
 # possible changes regarding custom tag system will follow here
 
-alias lsa='ls -gGach --time-style=+"%Y.%m.%d %H:%M:%S"'
+alias lsa='ls -gGach -N --time-style=+"%Y.%m.%d %H:%M:%S"'
 alias lsl='ls -l'
 
 alias cls='clear ; lsa'
