69817de5ba1e640a7b02519d7e1a0cd993e108b5
miomao34 <22483457+miomao34@users.noreply.github.com>
2020-12-24 22:53:00 +0300

    sysconfig installation now uses https for new systems

--- .config/upstart/update-sysconfig.sh
+++ .config/upstart/update-sysconfig.sh
@@ -4,7 +4,7 @@ cd ~
 rm -rf ./sysconfig/
 rm -rf ./.git
 
-git clone --no-checkout git@github.com:miomao34/sysconfig.git
+git clone --no-checkout https://github.com/miomao34/sysconfig.git
 mv ./sysconfig/.git .
 rm -rf ./sysconfig/
 
