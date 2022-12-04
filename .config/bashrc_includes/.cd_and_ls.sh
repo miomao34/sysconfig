6d3cbb371bfc25f9cc7031682e8e1df49c66555b
miomao34 <flex384@gmail.com>
2022-12-04 04:33:19 +0300

    look at me go, updating the repo! minor aliases cleanup, updated the install script, cleaning up and updating the .config folder

--- .config/bashrc_includes/.cd_and_ls.sh
+++ .config/bashrc_includes/.cd_and_ls.sh
@@ -16,28 +16,33 @@ lsa()
 
 alias cls='clear ; lsa'
 
-# lss and lsl switch between short and long modes of lsa in one terminal session; long is default
+# lss and lsl switch between the short and long modes of lsa and the prompt in the
+# current terminal session; long is the default
+# common lsa columns are replaced by yy.mm.dd and filename; SHORT_PS1 & LONG_PS1 replace PS1 prompt, with LONG_PS1 == PS1
+SHORT_PS1="\[\033[01;34m\]\w\[\033[00m\]\$ "
+LONG_PS1="${PS1}"
 alias lss='LSSHORT="ON"; PROMPT_DIRTRIM=1; export PS1="${SHORT_PS1}"; cls'
 alias lsl='unset LSSHORT; unset PROMPT_DIRTRIM; export PS1="${LONG_PS1}"; cls'
 
 # This cd clears screen and lists all files on directory change;
-# also, "cd +++" will translate to "cd ../../.."
+# also, "cd +++" will be translated to "cd ../../.."
 cdi()
 {
 	clear
 
 	backward_symbol="+"
+
 	symbols_count=$(awk -F"$backward_symbol" '{print NF-1}' <<< "${1}")
 	
-	if [ "$symbols_count" -ge "1" ] && [ -z "${1//\+}" ] && [ "$1" ]
+	if [ "$symbols_count" -ge "1" ] && [ -z "$(echo $1 | tr -d $backward_symbol)" ] && [ "$1" ]
 	then
 		current_dir="$(pwd)"
 		
-		for i in $(eval echo "{1..$symbols_count}"); do cd ..; done
+		for _ in $(seq $symbols_count); do cd ..; done
 		
 		export OLDPWD="$current_dir"
 	else	
-		cd "$@"
+		cd $@
 	fi
 
 	lsa
--- install.sh
+++ install.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/bash
 
-BACKUP_FOLDER="./$(date +%s)-backup"
+BACKUP_FOLDER="./$(date -Iseconds)-backup"
 
 # saving current settings if they exist
-SAVE_LIST=(".bashrc" ".gitconfig" ".vimrc" ".moc")
+SAVE_LIST=(	".bashrc"
+			".gitconfig"
+			".vimrc"
+			".moc"
+			".config/*"
+		)
 for file in ${SAVE_LIST[@]}
 do
-	echo -n "trying ${file}... "
+	echo -n "${file}: "
 	if ! [[ -f "${file}" || -d "${file}" ]]
 	then
-		echo -n "not found..."
+		echo -n "not found - "
 	else
-		echo -n "found, backing up..."
+		echo -n "found - "
 		mv "~/${file}" "${BACKUP_FOLDER}/${file}"
-		echo -n "backed up..."
+		echo -n "backed up - "
 	fi
 
-	echo -n "linking..."
+	echo -n "linking - "
 	ln -s "./${file}" "~/${file}"
 	echo "done!"
 done
