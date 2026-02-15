4ce2703831341a0b7c2d00a42d94ed1f4a6bbb30
miomao34 <flex384@gmail.com>
2026-02-15 19:16:02 +0300

    moc on song change notify script update, minor screenshot updates

--- .moc/scripts/on-song-change-notifier.sh
+++ .moc/scripts/on-song-change-notifier.sh
@@ -6,9 +6,14 @@ on_song_change_notify () {
     filename="${filepath##*\/}"
     containing_folder="${filepath%\/*}"
     
+    # looking for icon: if cover.* not found, then looking for subdirecrories; if still not found, then going up a level untill an image is found
     icon="$(find "${containing_folder}" -maxdepth 1 -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' | grep -i cover | head -n 1)"
-    icon="${icon:-$(find "${containing_folder}" -maxdepth 1 -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' | head -n 1)}"
-    #icon="$(find "${containing_folder}" -maxdepth 1 -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' | head -n 1)"
+    icon="${icon:-$(find "${containing_folder}" -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' | head -n 1)}"
+
+    while [ -z "$icon" ] && [ "${containing_folder}" != "/" ]; do
+        icon="$(find ${containing_folder} -maxdepth 1 -iname '*.png' -o -iname '*.jpg' -o -iname '*.jpeg' | head -n 1)"
+        containing_folder="${containing_folder%\/*}"
+    done
     
     length="$2"
 
